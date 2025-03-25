a614ce2f12867e781647dc2c7c401d0c4e050ab3
mkaipus <112018529+mkaipus@users.noreply.github.com>
2025-03-25 23:36:55 +0000

    updated

---
+++ project1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import re
+import os
+
+# load in dataset 
+hotel_data = pd.read_csv('/kaggle/input/trip-advisor-hotel-reviews/tripadvisor_hotel_reviews.csv')
+
+
+# hotel_data.head())
